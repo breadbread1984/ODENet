7ad39d4128936f2fdcdf8d92913f44800fd0660c
breadbread1984 <breadbread1984@163.com>
2019-01-11 11:41:32 +0800

    modified:   README.md 	modified:   odenet_tf.py

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ This project implements a simple demo of [ODENet](https://arxiv.org/abs/1806.073
 ## Introduction
 ODENet models the outputs of NN layers with a function f(x). In tradition network, layer index is within integer domain. ODENet extends the index into real number domain. The f(x) is defined by an ordinary differential equation df(x)/dx = F(f(x),x,theta). This demo use lorenz function as the f(x). The demo learns the parameter with ODENet algorithm.
 
-## Note
-The demo currently crash at odeint function due to its restriction on input time point. it requires the time points are provided in ascending order. I already filed for feature improvement [here](https://github.com/tensorflow/tensorflow/issues/24823). The demo is supposed to work after the odeint accepts unordered timepoints.
-
--- odenet_tf.py
+++ odenet_tf.py
@@ -5,12 +5,14 @@ import tensorflow as tf;
 
 class ODENet(object):
     
-    def __init__(self,F = None):
+    def __init__(self,F = None,theta = None):
         assert callable(F);
+        assert issubclass(type(theta), tf.Variable);
         self.F = F;
+        self.theta = theta;
 
-    def aug_F(self, aug_ft, t, theta = None):
-        assert issubclass(aug_ft, tf.Tensor) and aug_ft.get_shape()[0] == 4;
+    def aug_F(self, aug_ft, t):
+        assert issubclass(type(aug_ft), tf.Tensor) and aug_ft.get_shape()[0] == 4;
         # this function do the following calculation
         # d augmented f(t)/dt = augmented F(augmented f(t),t,theta)
         # inputs:
@@ -20,11 +22,9 @@ class ODENet(object):
         ft, dloss_dft, _, _ = tf.unstack(aug_ft);
         #get jacobian of F
         with tf.GradientTape() as g:
-            g.watch([ft, t, theta]);
-            F = self.F(ft,t,theta);
-        dF_dft = g.gradient(F,ft); #dF/df(t)
-        dF_dtheta = g.gradient(F,theta); #dF/dtheta
-        dF_dt = g.gradient(F,t); #dF/dt
+            g.watch([ft, t, self.theta]);
+            F = self.F(ft, t, self.theta);
+        [dF_dft,dF_dtheta,dF_dt] = g.gradient(F,[ft, self.theta, t]); #dF/df(t),dF/dtheta,dF/dt
         #get d augmented f(t) / dt
         da_dt = -dloss_dft * dF_dft; # da(t)/dt = -a(t)^T * dF/df(t)
         datheta_dt = -dloss_dft * dF_dtheta; # da_theta(t)/dt = -a(t)^T * dF/dtheta
@@ -33,7 +33,7 @@ class ODENet(object):
         # d augmented f(t) / dt which is [df(t)/dt = F, da(t)/dt, da_theta(t)/dt, da_t(t)/dt]
         return tf.stack([F, da_dt, datheta_dt, dat_dt]);
     
-    def gradient(self, yt, t, theta, aT = None):
+    def gradient(self, yt, t, aT = None):
         # inputs:
         # 1)yt is an array of f(t)
         # 2)t is an array of t where the supervised value f(t) is sampled
@@ -46,18 +46,20 @@ class ODENet(object):
         at = aT; #dloss/df(T-1) = dloss(f(T-1),tilde{f})/df(T-1)
         T = int(t.get_shape()[0]);
         sum_dloss_dt = 0;
-        dloss_dtheta = tf.zeros(theta.get_shape());
+        dloss_dtheta = tf.zeros(self.theta.get_shape());
         for i in range(T - 1, 0, -1):
             # -dloss / dt = -df(t) / dt * dloss / df(t) = F(f(t),t,theta) * (-dloss / df(t)) = F(f(t),t,theta) * a(t)
             # the real dloss_dt needs one more tf.math.reduce, it is not practiced to fit the dimension requirements of tf.stack
-            dloss_dt = at * self.F(yt[i], t[i], theta);
+            dloss_dt = at * self.F(yt[i], t[i]);
             # sum_{t=t}^{T-1} dloss / dt
             sum_dloss_dt = sum_dloss_dt - dloss_dt;
             # initial condition [f(t),a(t) = dloss/df(t), a_theta(t), a_t(t)]
             # get augmented func of the previous time [f(t-1), a(t-1), a_theta(t-1), a_t(t-1)]
             aug_ft = tf.stack([yt[i], at, dloss_dtheta, sum_dloss_dt]);
             #NOTE: tensorflow doesnt support odeint with time points given in descending order
-            aug_ftm1 = tf.contrib.integrate.odeint(self.aug_F, aug_ft, [t[i],t[i-1]]);
+            #df(x0)/dx = int_{x1}^{x0} F(f(x),x,theta) dx   (x0 < x1)  f(x1) is known
+            #df(x0)/dx = int_{-x1}^{-x0} -F(f(-z),-z,theta) dz  f(x1) is known
+            aug_ftm1 = tf.contrib.integrate.odeint(lambda ft,t:-self.aug_F(ft,t), aug_ft, [-t[i],-t[i-1]]);
             _, atm1, dloss_dtheta, sum_dloss_dtm1 = tf.unstack(aug_ftm1[1]);
             # update a(t-1)
             at = atm1;
@@ -74,20 +76,20 @@ if __name__ == "__main__":
         dz = x * y - beta * z;
         return tf.stack([dx, dy, dz]);
 
-    odenet = ODENet(lorenz);
+    #initial value of trainable parameter
+    theta = tf.Variable([28.0, 10.0, 8.0/3.0], dtype = tf.float32);
+    odenet = ODENet(lorenz,theta);
     #time points [x_0, ... , x_{T-1}]
     t = tf.constant(np.linspace(0, 10, num = 10), dtype = tf.float32);
     #samples [f(x_0), ..., f(x_{T-1})]
     yt = tf.constant(np.random.normal(size = (10,3)), dtype = tf.float32);
     #initial loss of the last timepoint -dloss/df(x_{T-1})
     aT = tf.constant(-np.ones([3]), dtype = tf.float32); #-dloss/df(x) is 3d vector
-    #initial value of trainable parameter
-    theta = tf.Variable([28.0, 10.0, 8.0/3.0], dtype = tf.float32);
     #learning rate
     lr = 1e-3;
     #Newton Descend
     while True:
-        dloss_dtheta = odenet.gradient(yt,t,theta,aT);
+        dloss_dtheta = odenet.gradient(yt,t,aT);
         print(dloss_dtheta);
         if tf.math.reduce(dloss_dtheta) < 1e-2: break;
         theta = theta - lr * dloss_dtheta;
